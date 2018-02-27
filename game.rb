4a57d0f3362d448488a70706dd065e66a0d75fe5
LeeSD90 <lee.doyle90@gmail.com>
2018-02-27 12:42:34 +0100

    Added get_random_code method to generate a random array with numbers corresponding to valid code colours

--- game.rb
+++ game.rb
@@ -1,6 +1,26 @@
 class Game
+	@@colors = ["R", "G", "B", "Y", "P", "C"]
+	
 
 	def initialize()
+		@code = get_random_code
+		puts @code
+	end
+
+	private
+
+	def get_random_code()
+		code = Array.new(4, -1)
+
+		code.each_with_index{ |v, i| 
+			assigning = true
+			while(assigning)
+				num = rand(0..5)
+				if(!code.include?(num)) then code[i] = num; assigning = false; end
+			end
+		}
+
+		return code
 	end
 
 end
