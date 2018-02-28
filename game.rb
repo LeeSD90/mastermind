48085a594b1ad4c02f41aa5d6520d63001c6de9b
LeeSD90 <lee.doyle90@gmail.com>
2018-02-28 14:58:04 +0100

    Added game loop and regex to check the formatting on a players guess

--- game.rb
+++ game.rb
@@ -1,13 +1,28 @@
 class Game
+require 'colorize'
 	@@colors = ["R", "G", "B", "Y", "P", "C"]
 	
 
 	def initialize()
 		@code = get_random_code
-		puts @code
+		play
 	end
 
 	private
+
+	def play()
+		puts "\nColors : " + "R".colorize(:red) + " G".colorize(:green) + " B".colorize(:blue) + " Y".colorize(:yellow) + " P".colorize(:light_magenta) + " C".colorize(:cyan)
+		12.times{
+			puts "\nGuess a 4-color combination\nFor example, to guess red, green, blue, yellow enter RGBY\n"
+			loop do
+				guess = gets.chomp.upcase!
+				if((guess =~ /^(?!.*(.).*\1)[RGBYPC]{4}$/) != nil) then 
+					
+					break
+				else puts "\nPlease enter your guess in the format RGBY using the available colors: "+ "R".colorize(:red) + " G".colorize(:green) + " B".colorize(:blue) + " Y".colorize(:yellow) + " P".colorize(:light_magenta) + " C".colorize(:cyan) end
+			end
+		}
+	end
 
 	def get_random_code()
 		code = Array.new(4, -1)
