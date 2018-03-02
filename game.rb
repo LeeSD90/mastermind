7555ac8a4ed6218a3b2e4deb65c74b11eb19576f
LeeSD90 <lee.doyle90@gmail.com>
2018-03-02 13:06:46 +0100

    Added a counter for number of guesses remaining

--- game.rb
+++ game.rb
@@ -13,16 +13,18 @@ require './logic.rb'
 
 	def play()
 		puts "\nColors : " + "R".colorize(:red) + " G".colorize(:green) + " B".colorize(:blue) + " Y".colorize(:yellow) + " P".colorize(:light_magenta) + " C".colorize(:cyan)
-		catch :success do
-			12.times{ #TODO CHANGE THIS LATER
-				puts "\nGuess a 4-color combination\nFor example, to guess red, green, blue, yellow enter RGBY\n"
+		puts "\nGuess a 4-color combination\nFor example, to guess red, green, blue, yellow enter RGBY\n"
+		i = 1
+		catch :success do	
+			12.times{
 				print @code #TODO And this
 				loop do
 					guess = gets.chomp.upcase!
 					if(Logic.check_guess(guess)) then
 						result = Logic.compare_guess(guess, @code)
 						throw :success if result[1] == 4
-						puts "\nColors correct: " + result[0].to_s + "\nPosition correct: " + result[1].to_s
+						puts "\nColors correct: " + result[0].to_s + "\nPosition correct: " + result[1].to_s + "\nGuesses remaining: " + (12 - i).to_s
+						i += 1
 						break
 					else puts "\nPlease enter your guess in the format RGBY using the available colors: "+ "R".colorize(:red) + " G".colorize(:green) + " B".colorize(:blue) + " Y".colorize(:yellow) + " P".colorize(:light_magenta) + " C".colorize(:cyan) 
 					end
